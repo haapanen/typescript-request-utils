eb777e4bfc4619eb249e86b4bc78ae1dda064b15
Jussi Haapanen <haapanen.jussi@gmail.com>
2020-10-08 22:11:35 +0300

    Use React.useCallback for the request callback
    Calling the request callback would've caused an infinite loop if used
    within React.useEffect callback.

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # typescript-request-utils
+
 Some utilities to simplify request handling on TypeScript
+
+# Installation
+
+npm install --save typescript-request-utils
+
+# Usage
+
+## OpenAPI typescript-fetch
+
+Example usage:
+
+```tsx
+function Component(props: {}) {
+  // You must use useMemo or initialize the API elsewhere
+  // if api is called in a useEffect. If not, a new API
+  // will be initialized whenever React renders the view
+  // causing an infinite loop
+  const api = React.useMemo(new ItemApi(), []);
+  api.getItems.bind(api);
+  const [itemsRequest, loadItems] = useOpenApiRequest(api.getItems);
+
+  if (itemsRequest.type === OpenApiRequestStatus.Pending) {
+    // itemsRequest has the following fields:
+    // startTime
+    // requestPayload
+    return <div>{itemsRequst.startTime}</div>;
+  } else if (itemsRequest.type === OpenApiRequestStatus.Complete) {
+    // itemsRequest has the following fields:
+    // startTime
+    // requestPayload
+    // endTime
+    // responsePayload
+    return <div>{itemsRequest.responsePayload[0].itemName}</div>;
+  } else if (itemsRequest.type === OpenApiRequestStatus.NetworkError) {
+    // itemsRequest has the following fields:
+    // startTime
+    // requestPayload
+    // endTime
+    // error: {
+    //     readonly message: string;
+    //     readonly stack: string;
+    // }
+    return <div>{itemsRequest.error.message}</div>;
+  } else if (itemsRequest.type === OpenApiRequestStatus.ServerError) {
+    // itemsRequest has the following fields:
+    // startTime
+    // requestPayload
+    // endTime
+    // error: Response (fetch response)
+    return <div>{itemsRequest.error.status}</div>;
+  }
+
+  return <button onClick={loadItems}>Load items</button>;
+}
+```
--- src/openapi-typescript-fetch/useOpenApiRequest.ts
+++ src/openapi-typescript-fetch/useOpenApiRequest.ts
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   OpenApiCompleteRequestState,
   OpenApiNetworkErrorRequestState,
@@ -28,53 +29,56 @@ export function useOpenApiRequest<
 ] {
   const [state, setState] = useOpenApiRequestState<TResponse, TParameters[0]>();
 
-  const request = async (...parameters: TParameters) => {
-    const startTime = new Date().toISOString();
-    try {
-      setState({
-        type: OpenApiRequestStatus.Pending,
-        startTime: startTime,
-        requestPayload: parameters,
-      });
+  const request = React.useCallback(
+    async (...parameters: TParameters) => {
+      const startTime = new Date().toISOString();
+      try {
+        setState({
+          type: OpenApiRequestStatus.Pending,
+          startTime: startTime,
+          requestPayload: parameters,
+        });
 
-      const response = await func(...parameters);
+        const response = await func(...parameters);
 
-      const state: OpenApiCompleteRequestState<TResponse, TParameters> = {
-        type: OpenApiRequestStatus.Complete,
-        startTime: startTime,
-        endTime: new Date().toISOString(),
-        requestPayload: parameters ? parameters[0] : undefined,
-        responsePayload: response,
-      };
-
-      setState(state);
-      return state;
-    } catch (err) {
-      // status only exists in the response if we successfully
-      // connected to the endpoint
-      if (typeof err.status === "number") {
-        const state: OpenApiServerErrorRequestState<TParameters> = {
-          type: OpenApiRequestStatus.ServerError,
+        const state: OpenApiCompleteRequestState<TResponse, TParameters> = {
+          type: OpenApiRequestStatus.Complete,
           startTime: startTime,
           endTime: new Date().toISOString(),
-          requestPayload: parameters,
-          error: err,
+          requestPayload: parameters ? parameters[0] : undefined,
+          responsePayload: response,
         };
+
         setState(state);
         return state;
-      } else {
-        const state: OpenApiNetworkErrorRequestState<TParameters> = {
-          type: OpenApiRequestStatus.NetworkError,
-          startTime: startTime,
-          endTime: new Date().toISOString(),
-          requestPayload: parameters,
-          error: err,
-        };
-        setState(state);
-        return state;
+      } catch (err) {
+        // status only exists in the response if we successfully
+        // connected to the endpoint
+        if (typeof err.status === "number") {
+          const state: OpenApiServerErrorRequestState<TParameters> = {
+            type: OpenApiRequestStatus.ServerError,
+            startTime: startTime,
+            endTime: new Date().toISOString(),
+            requestPayload: parameters,
+            error: err,
+          };
+          setState(state);
+          return state;
+        } else {
+          const state: OpenApiNetworkErrorRequestState<TParameters> = {
+            type: OpenApiRequestStatus.NetworkError,
+            startTime: startTime,
+            endTime: new Date().toISOString(),
+            requestPayload: parameters,
+            error: err,
+          };
+          setState(state);
+          return state;
+        }
       }
-    }
-  };
+    },
+    [func]
+  );
 
   return [state, request];
 }
